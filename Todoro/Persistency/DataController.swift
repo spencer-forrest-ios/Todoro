0c1767c659c631f972a8624d24d9ce43474fa86a
Spencer Forrest <spencer.forrest.ios@gmail.com>
2019-04-16 23:02:54 +0200

    Boiler plate for coredata.

---
+++ Todoro/Persistency/DataController.swift
@@ -0,0 +1,61 @@
+//
+//  DataController.swift
+//  Todoro
+//
+//  Created by Spencer Forrest on 16/04/2019.
+//  Copyright © 2019 Spencer Forrest. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+  
+  let persistentContainer: NSPersistentContainer
+  
+  var viewContext: NSManagedObjectContext {
+    return persistentContainer.viewContext
+  }
+  
+  init(modelName: String) {
+    persistentContainer = NSPersistentContainer(name: modelName)
+  }
+  
+  func load (completion: (() -> Void)? = nil) {
+    persistentContainer.loadPersistentStores {
+      (storeDescription, error) in
+      if let error = error as NSError? {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        
+        /*
+         Typical reasons for an error here include:
+         * The parent directory does not exist, cannot be created, or disallows writing.
+         * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+         * The device is out of space.
+         * The store could not be migrated to the current model version.
+         Check the error message to determine what the actual problem was.
+         */
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+      
+      if let completion = completion {
+        completion()
+      }
+    }
+  }
+  
+  func saveContext () {
+    let context = persistentContainer.viewContext
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
+    }
+  }
+}
